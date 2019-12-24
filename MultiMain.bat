48f1a18a54ab4c11886ad3734b65b33b87db9ee8
Luke Sullivan <52180830+Yuck31@users.noreply.github.com>
2019-12-24 02:11:05 -0500

    Multi-Thread Batch File for the game

---
+++ MultiMain.bat
@@ -0,0 +1,15 @@
+::This is the Batch File that executes the Game.
+
+::Opens Terminal and dosn't display input
+@echo off
+
+::Sets CLASSPATH
+set CLASSPATH=.
+
+::Compiles the Game class with Jamepad and OGG extensions
+javac -cp ".;jamepad.jar;EasyOgg/easyogg.jar;EasyOgg/jogg-0.0.7.jar;EasyOgg/jorbis-0.0.15.jar" JettersR/MultiMain.java
+
+::Runs the Game class with Jamepad and OGG extensions
+java -cp ".;jamepad.jar;EasyOgg/easyogg.jar;EasyOgg/jogg-0.0.7.jar;EasyOgg/jorbis-0.0.15.jar" JettersR/MultiMain
+
+pause
